b19a7f8d5ceeaa8de3b68c1a59e1ba42d6987af8
Samantha Cohen <samanthacohen294@gmail.com>
2017-10-29 12:37:03 +0000

    add wget!

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 pacman -Sy
 pacman -S zsh --noconfirm
+pacman -S wget --noconfirm
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -s .gitconfig ~/.gitconfig
