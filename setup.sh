044973113a43ff41746a3ebe2d2e24770ab29f8a
Samantha Cohen <samanthacohen294@gmail.com>
2017-10-29 12:39:44 +0000

    abs directory for sym link for gitconfig

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 pacman -Sy
 pacman -S zsh --noconfirm
 pacman -S wget --noconfirm
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s .gitconfig ~/.gitconfig
+ln -s $DIR/.gitconfig ~/.gitconfig
