896a798e9a3932df8c4ba99125f72105512bea9c
Samantha Cohen <samanthacohen294@gmail.com>
2017-10-29 12:32:42 +0000

    add more to setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+#! /bin/bash
+
 pacman -Sy
+pacman -S git
 pacman -S zsh
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+ln -s .gitconfig ~/.gitconfig
