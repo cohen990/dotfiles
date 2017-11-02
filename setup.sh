4feafca23a29d64dfcd5fb808f6f6fa2c3848c4d
Samantha Cohen <samanthacohen294@gmail.com>
2017-11-02 15:40:06 +0000

    add vimrc to setup script

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ pacman -S wget --noconfirm
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -s $DIR/.gitconfig ~/.gitconfig
+ln -s $DIR/.vimrc ~/.vimrc
