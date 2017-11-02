239a5c64ccf6bf4b4424e5ae04f1b6956a2e0aad
Samantha Cohen <samanthacohen294@gmail.com>
2017-11-02 20:13:48 +0000

    force the symlinks

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ pacman -S zsh --noconfirm
 pacman -S wget --noconfirm
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s $DIR/.gitconfig ~/.gitconfig
-ln -s $DIR/.vimrc ~/.vimrc
-ln -s $DIR/.zshrc ~/.zshrc
+ln -fs $DIR/.gitconfig ~/.gitconfig
+ln -fs $DIR/.vimrc ~/.vimrc
+ln -fs $DIR/.zshrc ~/.zshrc
+source ~/.zshrc
