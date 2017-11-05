11d46bcedc783610059e921d8b646088d7e99508
Samantha Cohen <samanthacohen294@gmail.com>
2017-11-05 15:04:01 +0000

    add x-server-stup scripts to configurations

---
+++ x-server-setup/setup.sh
@@ -0,0 +1,6 @@
+pacman -S xorg-server --noconfirm
+pacman -S lxde-common --noconfirm
+pacman -S lxsession --noconfirm
+pacman -S openbox --noconfirm
+pacman -S lxde --noconfirm
+pacman -S xorg-xinit --noconfirm
---
+++ x-server-setup/unsetup.sh
@@ -0,0 +1,6 @@
+pacman -Rs xorg-server --noconfirm
+pacman -Rs lxde-common --noconfirm
+pacman -Rs lxsession --noconfirm
+pacman -Rs openbox --noconfirm
+pacman -Rs lxde --noconfirm
+pacman -Rs xorg-xinit --noconfirm
