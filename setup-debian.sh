b7c16fd804cfa5de843a7342328efd5f2fe8005e
Samantha Cohen <samanthacohen294@gmail.com>
2017-11-26 13:59:11 +0000

    fix thefuck installation in debian

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,4 +3,6 @@
 sudo apt update
 sudo apt install zsh -y
 sudo apt install wget -y
-sudo apt-get install thefuck -y
+sudo apt install python3-dev python3-pip
+sudo pip3 --upgrade pip
+sudo pip3 install thefuck
