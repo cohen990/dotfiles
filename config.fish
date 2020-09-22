a2d42725d34b5b2d54f64543693faeae3ddc28ad
Samantha Cohen <samanthacohen294@gmail.com>
2020-09-22 14:11:52 +0100

    add fish config

---
+++ config.fish
@@ -0,0 +1,21 @@
+alias y=yarn
+alias tg=terragrunt
+alias tf=terraform
+alias c="code ."
+alias fixtime="sudo ntpd -qg"
+alias cci=circleci
+
+setxkbmap -layout gb
+setxkbmap -option 'caps:escape'
+
+export AWS_REGION=eu-west-1
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/serverless.fish ]; and . /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/sls.fish ]; and . /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/slss.fish ]; and . /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/slss.fish
