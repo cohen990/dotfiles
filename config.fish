6ed8542cd594cc5267a6f57669b60f1af2806f50
Samantha Cohen <samanthacohen294@gmail.com>
2021-10-11 14:41:08 +0100

    loads of updates

--- config.fish
+++ config.fish
@@ -6,6 +6,13 @@ alias fixtime="sudo ntpd -qg"
 alias cci=circleci
 alias icat="kitty +kitten icat --align left"
 alias iclip="xclip -sel clipboard -t image/png"
+alias splitpipe="tee (tty)"
+alias z=zoxide
+alias togit="cd /home/git"
+
+function puml-watch 
+    ls $argv.puml | entr -s "plantuml $argv.puml && icat $argv.png"
+end
 
 setxkbmap -layout gb
 setxkbmap -option 'caps:escape'
@@ -14,6 +21,9 @@ xset b off
 xset b 0 0 0
 
 export AWS_REGION=eu-west-1
+export AWS_DEFAULT_REGION=eu-west-1
+
+export PATH="/home/dan/.local/bin:$PATH"
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
@@ -24,3 +34,10 @@ export AWS_REGION=eu-west-1
 # tabtab source for slss package
 # uninstall by removing these lines or running `tabtab uninstall slss`
 [ -f /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/slss.fish ]; and . /home/dan/git/cazoo/monitoring-and-alarms-automation/node_modules/tabtab/.completions/slss.fish
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
+# Inits
+zoxide init fish | source
+status is-interactive; and pyenv init --path | source
--- setup.sh
+++ setup.sh
@@ -5,9 +5,8 @@ ln -fs `pwd`/.gitconfig ~/.gitconfig
 ln -fs `pwd`/.vimrc ~/.vimrc
 ln -fs `pwd`/.git-commit-template ~/.git-commit-template
 
-mkdir ~/.config
-mkdir ~/.config/i3
-mkdir ~/.config/fish
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/fish
 
 ln -fs `pwd`/config.fish ~/.config/fish/config.fish
 ln -fs `pwd`/i3.config ~/.config/i3/config
