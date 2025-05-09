7f883c96fcf0f2b3ce80b01b2067214fe1c38562
Samantha Cohen <samantha.cohen@ovo.com>
2025-05-09 15:40:21 +0100

    expanding the brew packages to support nvim more fully - fullier

--- nvim/lua/plugins/fathom.lua
+++ nvim/lua/plugins/fathom.lua
@@ -5,5 +5,16 @@ return {
     opts = {
       image = {},
     },
-  }
+  },
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        border = "none",
+        width = "full",
+      },
+    },
+  },
 }
