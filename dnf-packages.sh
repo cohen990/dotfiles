8bf12f98dbaec5a5c6b8e97306c04acd682b3056
Samantha Cohen <samantha.cohen@ovo.com>
2025-05-29 19:10:16 +0100

    adds personal computer dependencies (steam)

--- dnf-packages.sh
+++ dnf-packages.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+case "$1" in
+    --personal|-p)
+        echo "Installing for a personal computer"
+        personal=true
+        ;;
+esac
+
 repos=()
 
 sudo dnf install -y dnf-plugins-core
@@ -17,6 +24,13 @@ packages+=("fira-code-fonts")
 packages+=("readline-devel")
 packages+=("bat")
 packages+=("zsh")
+
+if [[($personal)]] ; then
+    echo "Adding dependencies for a personal computer"
+    packages+=("https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm")
+    packages+=("https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm")
+    packages+=("steam")
+fi
 
 sudo dnf config-manager addrepo --from-repofile=${repos[*]}
 sudo dnf install -y ${packages[*]}
